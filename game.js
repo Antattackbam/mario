ac2be79805394f4a21c8809855469f0124f28bbc
Anthony Rodriguez <antcreatesmagic@gmail.com>
2021-08-27 16:40:52 -0500

    surprise blocks/ enemy movements/ death

--- game.js
+++ game.js
@@ -7,7 +7,11 @@ kaboom({
 })
 
 const MOVE_SPEED = 120
+const ENEMY_SPEED = 20
 const JUMP_FORCE = 460
+const BIG_JUMP_FORCE = 550
+let CURRENT_JUMP_FORCE = JUMP_FORCE
+
 
 loadRoot('https://i.imgur.com/')
 loadSprite('coin', 'wbKxhcd.png')
@@ -40,7 +44,7 @@ scene('game', () =>{
         '                   $   $                                        ',
         '               $     ^    $                                     ',
         '       $             =           #*                             ',
-        '         ===    =          =                                    ',
+        '   #*      ===    =          =                                  ',
         '                                                                ',
         '                                                                ',
         '                                           -+                   ',
@@ -52,7 +56,7 @@ scene('game', () =>{
         width: 20,
         height: 20,
         '=': [sprite('block'), solid(), area()],
-        '$': [sprite('coin')],
+        '$': [sprite('coin'), 'coin'],
         '#': [sprite('surprise'), solid(), area(), 'coin-surprise'],
         '*': [sprite('surprise'), solid(), area() , 'mushroom-surprise'],
         '}': [sprite('unboxed'), solid(), area()],
@@ -60,35 +64,100 @@ scene('game', () =>{
         ')': [sprite('pipe-bottom-right'), solid(), area()],
         '-': [sprite('pipe-top-left'), solid(), area()],
         '+': [sprite('pipe-top-right'), solid(), area()],
-        '^': [sprite('mushroom')],
-        '%': [sprite('evil-shroom',)]
+        '^': [sprite('mushroom'), 'mushroom', body(), area()],
+        '%': [sprite('evil-shroom'), solid(), area(), 'dangerous']
     }
 
     const gameLevel = addLevel(map, levelCfg)
 
     const scoreLabel = add([
-        text("Kirby is the best <3"),
+        text("test"),
         pos(30,6),
         layer('ui'),
         {
-            value: "Kirby is the best <3",
+            value: "test",
         }
     ])
 
     add([text('level' + 'test', pos(4,6))])
 
+    function big() {
+        let timer = 0
+        let isBig = false
+        return {
+            update(){
+                if (isBig){
+                    timer -= dt()
+                    if (timer <= 0){
+                        this.smallify()
+                    }
+                }
+            },
+            isBig(){
+                return isBig
+            },
+            smallify(){
+                this.scale = vec2(1)
+                timer = 0
+                isBig = false
+            },
+            biggify(time){
+                this.scale = vec2(2)
+                CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+                timer = time
+                isBig = true
+            }
+        }
+    }
+
     const player = add([
         sprite('mario'), area(), solid(),
         pos(30,5),
         body(),
+        big(),
         origin('bot')
     ])
+
+    action('mushroom', (m) => {
+        m.move(13, 0)
+    })
+
+    player.on("headbutt", (obj) =>{
+        if (obj.is('coin-surprise')){
+            gameLevel.spawn('$', obj.gridPos.sub(0,1))
+            destroy(obj)
+            gameLevel.spawn('}', obj.gridPos.sub(0,0))
+        }
+        if (obj.is('mushroom-surprise')){
+            gameLevel.spawn('^', obj.gridPos.sub(0,1))
+            destroy(obj)
+            gameLevel.spawn('}', obj.gridPos.sub(0,0))
+        }
+    })
 
 
     player.action( () => {
         player.pushOutAll();
     })
 
+    action('dangerous', (d) => {
+        d.move(-ENEMY_SPEED, 0)
+    })
+
+    player.collides('mushroom', (m) => {
+        destroy(m)
+        player.biggify(6)
+    })
+
+    player.collides('dangerous', () => {
+        go('lose', {score: scoreLabel.value})
+    })
+
+    player.collides('coin', (c) => {
+        destroy(c)
+        scoreLabel.value++
+        scoreLabel.text = scoreLabel.value
+    })
 
 
     keyDown('left', () => {
@@ -104,11 +173,15 @@ scene('game', () =>{
 
     keyPress('space', () => {
         if(player.grounded()) {
-            player.jump(JUMP_FORCE)
+            player.jump(CURRENT_JUMP_FORCE)
         }
     })
     
  
+})
+
+scene('lose', ({score}) => {
+    add([text(score, 32), origin('center'), pos(width()/2, height()/2)])
 })
 
 go('game')
